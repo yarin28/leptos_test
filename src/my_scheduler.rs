fed5f3c7e74e18983e54d903b8e72fce1ba98203
yarinLaptop <yarin.greenfeld@gmail.com>
2023-08-09 17:06:45 +0300

    wip on changing duration

--- src/my_scheduler.rs
+++ src/my_scheduler.rs
@@ -33,6 +33,7 @@ async fn create_water_pump_job() -> Result<Job> {
     let mut jj = Job::new_repeated_async(Duration::from_secs(8), move |uuid, mut l| {
         {
             Box::pin(async move {
+                // l.context.metadata_storage.read().await.get(uuid);
                 info!(
                     "inside the water pump job and the cron string is - {:?}",
                     CONFIG.lock().await
@@ -67,11 +68,21 @@ pub async fn lunch_the_watering_schedualed_program() -> Result<()> {
     }
     let mut sched = JobScheduler::new().await?;
     let mut water_pump_job = create_water_pump_job().await.unwrap();
+    let water_pump_job_uuid = water_pump_job.guid().clone();
     sched.add(water_pump_job).await?;
     info!(
         "the cron string is - {:?}",
         CONFIG.lock().await.cron_string // CONFIG.blocking_lock().cron_string
     );
+
+    sched
+        .context
+        .metadata_storage
+        .read()
+        .await.set_next_and_last_tick(water_pump_job_uuid, Ok(Duration::from_secs(10)), Duration::from_secs(11))//FIXME: needs to use chrono::duration::Duration
+        // .get(water_pump_job_uuid)
+        // .await;
+        ;
     // sched
     //     .add(Job::new_async(env!("CRON_STRING"), move |uuid, mut l| {
     //         Box::pin(async move {
@@ -105,7 +116,10 @@ pub async fn lunch_the_watering_schedualed_program() -> Result<()> {
                     % 60;
                 let new_str = format!("{}{}{}", "1/", current_seconds_of_minute, " * * * * * * ");
                 file_config.cron_string = new_str.clone();
-                println!("{:?}", water_pump_job.job_data());
+                // TODO: this currently is the place that i want to issue the command to change the
+                // duration of the water pump job, there could be a better way.
+                // i beleve that i need to change the job data but i cant borrow the job vaiable
+                // println!("{:?}", water_pump_job.job_data());
                 // Query the next execution time for this job
                 let next_tick = l.next_tick_for_job(uuid).await;
                 match next_tick {
