3ebf306240f22917129ea9f2d9f3882bb4e85192
yarinLaptop <yarin.greenfeld@gmail.com>
2023-10-22 15:20:55 +0300

    wip better function seperation

--- src/utils/config_builder.rs
+++ src/utils/config_builder.rs
@@ -39,20 +39,16 @@ trait FromLuaTable {
 #[derive(Debug, Clone)]
 pub struct MyFormat;
 
-fn get_lua_table_with_struct<T: Default, FromLuaTable>(
-    table_name_in_lua: &str,
-    lua_ctx: Context,
-) -> T {
-    trait FromLuaTable {
-        fn from_lua_table(table: &rlua::Table) -> Self;
-    }
-    let lua = rlua::Lua::new();
-    let mut result = T::default();
+fn get_lua_table_with_struct<T>(table_name_in_lua: &str, lua_ctx: &Context) -> T
+where
+    T: Default + FromLuaTable,
+{
+    let result: T;
     let table = match lua_ctx.globals().get(table_name_in_lua) {
         Ok(table) => {
             event!(
                 tracing::Level::DEBUG,
-                "got the gpio_table variable from lua and the value is ->{:?}",
+                "got the {table_name_in_lua:} variable from lua and the value is ->{:?}",
                 &table
             );
             T::from_lua_table(&table)
@@ -84,37 +80,20 @@ impl Format for MyFormat {
         //
         let mut result = Map::new();
         let lua = rlua::Lua::new();
-        let mut config_from_lua = GpioConfig::default();
+        let mut gpio_config_from_lua = GpioConfig::default();
+        let mut gpio2_config: rlua::Table;
 
         lua.context(|lua_ctx| {
             lua_ctx.load(text).exec().unwrap();
-
-            let table = match lua_ctx.globals().get("gpio_table") {
-                Ok(table) => {
-                    event!(
-                        tracing::Level::DEBUG,
-                        "got the gpio_table variable from lua and the value is ->{:?}",
-                        &table
-                    );
-                    GpioConfig::from_lua_table(&table)
-                }
-                Err(e) => {
-                    event!(
-                        tracing::Level::ERROR,
-                        "coulden`t the gpio_table variable from lua and the error is ->{:?}",
-                        &e
-                    );
-                    GpioConfig::default()
-                }
-            };
-            config_from_lua = table;
+            gpio_config_from_lua = get_lua_table_with_struct::<GpioConfig>("gpio_table", &lua_ctx);
+            gpio2_config = lua_ctx.globals().get("gpio_table2").unwrap();
         });
         event!(
             tracing::Level::DEBUG,
             "this is the config ->{:?}",
-            &config_from_lua
+            &gpio_config_from_lua
         );
-        let json_string = serde_json::to_string(&config_from_lua)?;
+        let json_string = serde_json::to_string(&gpio_config_from_lua)?;
         dbg!(&json_string);
         let lookup: HashMap<String, Value> = serde_json::from_str(&json_string)?;
         dbg!(&lookup);
