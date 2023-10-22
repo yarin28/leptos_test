ebed8b7ade37cbcd454bcb5777c04ba275b67ef7
yarinLaptop <yarin.greenfeld@gmail.com>
2023-10-22 14:15:48 +0300

    WIP

--- src/utils/config_builder.rs
+++ src/utils/config_builder.rs
@@ -2,9 +2,10 @@ use std::io::prelude::*;
 use std::{collections::HashMap, fs::File};
 // use anyhow::Result;
 use config::{Config, FileStoredFormat, Format, Map, Value, ValueKind};
-use rlua::{Function, Lua, MetaMethod, Result, UserData, UserDataMethods, Variadic};
+use rlua::{Context, Function, Lua, MetaMethod, Result, UserData, UserDataMethods, Variadic};
 use rlua_table_derive::FromLuaTable;
-use serde::Serialize;
+use serde::{de, Serialize};
+use tracing::event;
 
 pub fn config_build() {
     let mut config_file_content: String = String::new();
@@ -23,7 +24,7 @@ pub fn config_build() {
     }
 }
 #[derive(Serialize, Default, Clone, FromLuaTable, Debug)]
-pub struct Settings {
+pub struct GpioConfig {
     name: String,
     gpio_pin: usize,
     gpio_type: String,
@@ -37,6 +38,37 @@ trait FromLuaTable {
 
 #[derive(Debug, Clone)]
 pub struct MyFormat;
+
+fn get_lua_table_with_struct<T: Default, FromLuaTable>(
+    table_name_in_lua: &str,
+    lua_ctx: Context,
+) -> T {
+    trait FromLuaTable {
+        fn from_lua_table(table: &rlua::Table) -> Self;
+    }
+    let lua = rlua::Lua::new();
+    let mut result = T::default();
+    let table = match lua_ctx.globals().get(table_name_in_lua) {
+        Ok(table) => {
+            event!(
+                tracing::Level::DEBUG,
+                "got the gpio_table variable from lua and the value is ->{:?}",
+                &table
+            );
+            T::from_lua_table(&table)
+        }
+        Err(e) => {
+            event!(
+                tracing::Level::ERROR,
+                "coulden`t the gpio_table variable from lua and the error is ->{:?}",
+                &e
+            );
+            T::default()
+        }
+    };
+    result = table;
+    result
+}
 
 impl Format for MyFormat {
     fn parse(
@@ -52,34 +84,45 @@ impl Format for MyFormat {
         //
         let mut result = Map::new();
         let lua = rlua::Lua::new();
-        let from_lua = Settings::default();
+        let mut config_from_lua = GpioConfig::default();
 
         lua.context(|lua_ctx| {
             lua_ctx.load(text).exec().unwrap();
 
-            let default = Settings::default();
-            let table = lua_ctx.globals().get("gpio_table").unwrap();
-            let from_lua = Settings::from_lua_table(&table);
+            let table = match lua_ctx.globals().get("gpio_table") {
+                Ok(table) => {
+                    event!(
+                        tracing::Level::DEBUG,
+                        "got the gpio_table variable from lua and the value is ->{:?}",
+                        &table
+                    );
+                    GpioConfig::from_lua_table(&table)
+                }
+                Err(e) => {
+                    event!(
+                        tracing::Level::ERROR,
+                        "coulden`t the gpio_table variable from lua and the error is ->{:?}",
+                        &e
+                    );
+                    GpioConfig::default()
+                }
+            };
+            config_from_lua = table;
         });
-        dbg!(from_lua);
-        let mut json_string = serde_json::to_string(&from_lua)?;
-        dbg!(json_string);
-        let mut lookup: HashMap<String, Value> = serde_json::from_str(&json_string)?;
-        dbg!(lookup);
+        event!(
+            tracing::Level::DEBUG,
+            "this is the config ->{:?}",
+            &config_from_lua
+        );
+        let json_string = serde_json::to_string(&config_from_lua)?;
+        dbg!(&json_string);
+        let lookup: HashMap<String, Value> = serde_json::from_str(&json_string)?;
+        dbg!(&lookup);
         result.insert(
             "pump".to_string(),
             Value::new(uri, ValueKind::Table(lookup)), //TODO: the struct will
                                                        //have to become a hashmap
         );
-        if text == "good" {
-            result.insert(
-                "key".to_string(),
-                Value::new(uri, ValueKind::String(text.into())),
-            );
-        } else {
-            println!("Something went wrong in {:?}", uri);
-        }
-
         Ok(result)
     }
 }
