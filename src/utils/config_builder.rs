e2512337845e84c5422b602a21b345903e00beb2
Yarin <yarin.greenfeld@gmail.com>
2023-10-23 22:36:39 +0300

    wip cant figure lua context

--- src/utils/config_builder.rs
+++ src/utils/config_builder.rs
@@ -2,7 +2,10 @@ use std::io::prelude::*;
 use std::{collections::HashMap, fs::File};
 // use anyhow::Result;
 use config::{Config, FileStoredFormat, Format, Map, Value, ValueKind};
-use rlua::{Context, Function, Lua, MetaMethod, Result, UserData, UserDataMethods, Variadic};
+use rlua::{
+    Context, FromLua, Function, Lua, MetaMethod, RegistryKey, Result, Table, UserData,
+    UserDataMethods, Variadic,
+};
 use rlua_table_derive::FromLuaTable;
 use serde::{de, Serialize};
 use tracing::event;
@@ -13,7 +16,7 @@ pub fn config_build() {
         .unwrap()
         .read_to_string(&mut config_file_content)
         .unwrap();
-    println!("this is the config text - {config_file_content}");
+    // println!("this is the config text - {config_file_content}");
     let config = Config::builder()
         .add_source(config::File::from_str(&config_file_content, MyFormat))
         .build();
@@ -30,6 +33,10 @@ pub struct GpioConfig {
     gpio_type: String,
     active_seconds: usize,
     cron_string: String,
+}
+#[derive(Serialize, Default, Clone, FromLuaTable, Debug)]
+pub struct GpioConfigVec {
+    array: Vec<GpioConfig>,
 }
 
 trait FromLuaTable {
@@ -81,22 +88,24 @@ impl Format for MyFormat {
         let mut result = Map::new();
         let lua = rlua::Lua::new();
         let mut gpio_config_from_lua = GpioConfig::default();
-        let mut gpio2_config: rlua::Table;
+        let mut gpio2_config: HashMap<String, Table> = HashMap::new();
+        let mut gpio_list: Vec<GpioConfig>;
 
         lua.context(|lua_ctx| {
             lua_ctx.load(text).exec().unwrap();
             gpio_config_from_lua = get_lua_table_with_struct::<GpioConfig>("gpio_table", &lua_ctx);
-            gpio2_config = lua_ctx.globals().get("gpio_table2").unwrap();
+            gpio_list = get_lua_table_with_struct("gpio_table2", &lua_ctx);
         });
         event!(
             tracing::Level::DEBUG,
             "this is the config ->{:?}",
             &gpio_config_from_lua
         );
+        dbg!(&gpio2_config);
         let json_string = serde_json::to_string(&gpio_config_from_lua)?;
-        dbg!(&json_string);
+        // dbg!(&json_string);
         let lookup: HashMap<String, Value> = serde_json::from_str(&json_string)?;
-        dbg!(&lookup);
+        // dbg!(&lookup);
         result.insert(
             "pump".to_string(),
             Value::new(uri, ValueKind::Table(lookup)), //TODO: the struct will
