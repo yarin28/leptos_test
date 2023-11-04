62d79f5236cc7f7b02102dce1ee3c82b6c9b2734
Yarin <yarin.greenfeld@gmail.com>
2023-11-04 12:09:27 +0200

    the config file parsing works!

--- src/utils/config_builder.rs
+++ src/utils/config_builder.rs
@@ -14,15 +14,14 @@ pub fn config_build() {
         .unwrap()
         .read_to_string(&mut config_file_content)
         .unwrap();
-    // println!("this is the config text - {config_file_content}");
     let config = Config::builder()
         .add_source(config::File::from_str(&config_file_content, MyFormat))
         .build();
 
-    // matth config {
-    //     Ok(cfg) => println!("A config: {:#?}", cfg),
-    //     Err(e) => println!("An error: {}", e),
-    // }
+    match config {
+        Ok(cfg) => println!("A config: {:#?}", cfg),
+        Err(e) => println!("An error: {}", e),
+    }
 }
 #[derive(Serialize, Default, Clone, FromLuaTable, Debug)]
 pub struct GpioConfig {
@@ -44,45 +43,6 @@ trait FromLuaTable {
 #[derive(Debug, Clone)]
 pub struct MyFormat;
 
-fn get_lua_table_with_struct<T>(table_name_in_lua: &str, lua_ctx: &Context) -> T
-where
-    T: Default + FromLuaTable,
-{
-    let result: T;
-    let table = match lua_ctx.globals().get(table_name_in_lua) {
-        Ok(table) => {
-            event!(
-                tracing::Level::DEBUG,
-                "got the {table_name_in_lua:} variable from lua and the value is ->{:?}",
-                &table
-            );
-            T::from_lua_table(&table)
-        }
-        Err(e) => {
-            event!(
-                tracing::Level::ERROR,
-                "coulden`t the gpio_table variable from lua and the error is ->{:?}",
-                &e
-            );
-            T::default()
-        }
-    };
-    result = table;
-    result
-}
-// a small explantaion on the lifetime-
-// i promise using the lifetime 'a  that both the lua table and the lua context have the same
-// "lifetime"
-fn from_lua_table_to_hash_map<'a>(
-    lua_value: rlua::Value<'a>,
-    rust_table: &mut HashMap<config::Value, config::Value>,
-    lua_ctx: Context<'a>,
-) -> Result<String> {
-    // let lua_value: rlua::Value<'a> = <rlua::Value<'a>>::from_lua(lua_value, lua_ctx).unwrap();
-    // dbg!(lua_value);
-    Ok("good".to_string())
-}
-
 impl Format for MyFormat {
     fn parse(
         &self,
@@ -90,66 +50,16 @@ impl Format for MyFormat {
         text: &str,
     ) -> std::result::Result<Map<String, config::Value>, Box<dyn std::error::Error + Send + Sync>>
     {
-        // Let's assume our format is somewhat malformed, but this is fine
-        // In real life anything can be used here - nom, serde or other.
-        //
-        // For some more real-life examples refer to format implementation within the library code
-        //
-        let mut result = Map::new();
+        let mut result = HashMap::new();
         let lua = rlua::Lua::new();
-        let mut gpio_config_from_lua = GpioConfig::default();
-        let mut gpio2_config: HashMap<String, Table> = HashMap::new();
-        let mut gpio_list: Vec<GpioConfig>;
-        // let mut lua_config: HashMap<rlua::String, Value> = HashMap::new();
-        let mut lua_config: HashMap<config::Value, config::Value> = HashMap::new();
-        println!("inside parse");
 
         lua.context(|lua_ctx| {
-            let config: Table = lua_ctx.load(text).eval().unwrap();
-            println!("tins is config return structure{:?}", config);
-            // lua_config = FromLua::from_lua(rlua::Value::Table(config), lua_ctx).unwrap();
-            let res = from_lua_table_to_hash_map(
-                rlua::Value::Table(config),
-                &mut lua_config,
-                lua_ctx.clone(),
+            let config_lua_table = lua_ctx.load(text).eval().unwrap();
+            result.insert(
+                uri.unwrap_or(&"lua".to_string()).to_string(),
+                lua_to_config_value(config_lua_table).unwrap(),
             );
-            match res {
-                Ok(_) => {}
-                Err(_) => event!(
-                    tracing::Level::ERROR,
-                    "there was an error with from_lua_table_to_hash_map"
-                ),
-            };
-
-            // config
-            //     .pairs::<String, rlua::Value>()
-            //     .for_each(|pair| { println!("this is pair - {pair:?}");
-            // if pair.});
-            // let globals:  =
-            println!("insidecontext");
-            // lua_ctx
-            //     .globals()
-            //     .pairs::<String, rlua::Value>()
-            //     .for_each(|pair| println!("this is pair - {pair:?}"));
-            // result.insert("luaconf", Value::new(uri, ValueKind::Table(globals)));
-            // gpio_config_from_lua = get_lua_table_with_struct::<GpioConfig>("gpio_table", &lua_ctx);
-            // gpio_list = get_lua_table_with_struct("gpio_table2", &lua_ctx);
         });
-        event!(
-            tracing::Level::DEBUG,
-            "this is the config ->{:?}",
-            &gpio_config_from_lua
-        );
-        dbg!(&gpio2_config);
-        let json_string = serde_json::to_string(&gpio_config_from_lua)?;
-        // dbg!(&json_string);
-        // let lookup: HashMap<String, Value> = serde_json::from_str(&json_string)?;
-        // dbg!(&lookup);
-        // result.insert(
-        //     "pump".to_string(),
-        //     config::Value::new(uri, ValueKind::Table(lookup)), //TODO: the struct will
-        //                                                        //have to become a hashmap
-        // );
         Ok(result)
     }
 }
@@ -161,4 +71,36 @@ impl FileStoredFormat for MyFormat {
     fn file_extensions(&self) -> &'static [&'static str] {
         &MY_FORMAT_EXT
     }
+}
+
+fn lua_to_config_value(lua_value: rlua::Value) -> Result<config::Value> {
+    Ok(match lua_value {
+        rlua::Value::Table(table) => {
+            let pairs = table.pairs();
+            let map = pairs
+                .map(|pair| {
+                    let (key, value) = pair?;
+                    let key = match key {
+                        rlua::Value::String(name) => name.to_str()?.to_string(),
+                        rlua::Value::Integer(i) => i.to_string(),
+                        _ => return Err(todo!()),
+                    };
+                    Ok((key, lua_to_config_value(value)?))
+                })
+                .collect::<Result<Vec<_>>>()?
+                .into_iter()
+                .collect::<HashMap<_, _>>();
+            config::Value::new(None, map)
+        }
+        rlua::Value::Nil => config::Value::new(None, config::ValueKind::Nil),
+        rlua::Value::Boolean(val) => config::Value::new(None, val),
+        rlua::Value::Integer(val) => config::Value::new(None, val),
+        rlua::Value::Number(val) => config::Value::new(None, val),
+        rlua::Value::String(val) => config::Value::new(None, val.to_str()?.to_string()),
+        rlua::Value::LightUserData(_) => todo!(),
+        rlua::Value::Function(_) => todo!(),
+        rlua::Value::Thread(_) => todo!(),
+        rlua::Value::UserData(_) => todo!(),
+        rlua::Value::Error(_) => todo!(),
+    })
 }
