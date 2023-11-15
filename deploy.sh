31f7bfa5205317ca5c670a317f83ff054bc837da
Yarin <yarin.greenfeld@gmail.com>
2023-11-15 19:23:14 +0200

    correct cron string(hours instead minuets )

--- deploy.sh
+++ deploy.sh
@@ -15,6 +15,7 @@ readonly SOURCE_PATH_BIN=./target/server/aarch64-unknown-linux-gnu/release/lepto
 readonly SOURCE_PATH_SITE=./target/site
 readonly ENV_VARIABLES="LEPTOS_OUTPUT_NAME="leptos_start" LEPTOS_SITE_ROOT="site" LEPTOS_SITE_PKG_DIR="pkg" LEPTOS_SITE_ADDR="0.0.0.0:3000" LEPTOS_RELOAD_PORT="3001" LEPTOS_ASSETS_DIR="assets" LEPTOS_STYLE_FILE="output.css""
 readonly COMMAND_TO_RUN_ON_TARGRT="cd ${TARGET_PATH_DIR}&& bash -c \"${ENV_VARIABLES} ${TARGET_PATH_BIN}\""
+
 cargo leptos build --release
 rsync ${SOURCE_PATH_BIN} ${SSH_HOST}:${TARGET_PATH}
 rsync -r ${SOURCE_PATH_SITE} ${SSH_HOST}:${TARGET_PATH}
